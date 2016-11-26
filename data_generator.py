636f02fb00182382ce8cde7bda8e7b4c8150e461
Alexander Pushin <alpush@yandex-team.ru>
2016-11-26 22:51:01 +0200

    Improve application
    It's still raw. I don't care what you think about it, asshole.

--- data_generator.py
+++ data_generator.py
@@ -3,19 +3,7 @@ import pickle
 import random
 
 class ImageSample:
-    """
-        Includes all the relevant information about the piece of art to use
-        in a game.
-        
-        Fields:
-        author - First and second name of author.
-        title - Name of the piece of art.
-        year - Year of creation.
-        wrong_authors - two authors that will be used as alternative answers.
-        picture_uri - link to the web page containing information about the piece of art.
-        img_link - link to the image itself.
-    """
-    def __init__(self, img_sample, wrong_authors):
+    def __init__(self, img_sample, wrong_authors, cur_score):
         self.title = img_sample['title']
         self.year = img_sample['year']
         authors = []
@@ -27,22 +15,22 @@ class ImageSample:
         self.authors = authors[:self.correct_id] + authors[self.correct_id:]
         self.picture_uri = img_sample['site']
         self.img_link = img_sample['uri']
+        self.cur_score = cur_score
     
     def get_qr_code_image(self):
         return qrcode.make(self.picture_uri)
 
 
 class SampleGenerator:
-    """ Generates random image data sample. """
     def __init__(self):
         self.image_data = pickle.load(open('data/images.pickle', 'rb'))
-        self.authors = frozenset([x['creator'] for x in image_data])
+        self.authors = frozenset([x['creator'] for x in self.image_data])
     
-    def get_random_sample(self):
+    def get_random_sample(self, cur_score=0):
         img_id = random.randint(0, len(self.image_data))
         wrong_authors = random.sample(self.authors - frozenset([self.image_data[img_id]['creator']]), 2)
-        return ImageSample(self.image_data[img_id], wrong_authors)
+        return ImageSample(self.image_data[img_id], wrong_authors, cur_score)
 
-#gen = SampleGenerator()
-#sample = gen.get_random_sample()
+# gen = SampleGenerator()
+# sample = gen.get_random_sample()
 
--- requests.py
+++ requests.py
@@ -1,5 +1,66 @@
+import os
+import uuid
 import tornado
+import pickle
+from data_generator import SampleGenerator
+
+
+TOP_PATH = 'data/top_score.pickle'
+
 
 class Game(tornado.web.RequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sample_generator = SampleGenerator()
+        self.questions = dict()
+        if not os.path.exists(TOP_PATH):
+            pickle.dump(0, TOP_PATH)
+
+        self.top_score = pickle.load(TOP_PATH)
+
+    def _get_question_id():
+        return str(uuid.uuid4())
+
+    def _update_top_score(score):
+        self.top_score = max(score, self.top_score)
+        pickle.dump(self.top_score, TOP_PATH)
+
     def get(self):
-        self.render('pages/game.html')
+        question_id = self._get_question_id()
+        self.questions[question_id] = self.sample_generator.get_random_sample()
+
+        self.render(
+            'pages/game.html',
+            img_link=self.questions[question_id].img_link,
+            title=self.questions[question_id].title,
+            year=self.questions[question_id].year,
+            question_id=question_id,
+            authors=enumerate(self.questions[question_id].authors),
+            cur_score=0,
+            top_score=self.top_score,
+        )
+
+    def post(self):
+        answer_id, question_id = self.get_argument("value").split(';')
+
+        if self.questions[question_id].correct_id == answer_id:
+            new_question_id = self._get_question_id()
+            self.questions[new_question_id] = self.sample_generator.get_random_sample(self.questions[question_id].cur_score + 1)
+            self.write(json.dumps({
+                    "verdict": "OK",
+                    "img_link": self.questions[question_id].img_link,
+                    "title": self.questions[question_id].title,
+                    "year": self.questions[question_id].year,
+                    "authors": self.questions[question_id].authors,
+                    'cur_score': self.questions[question_id].cur_score,
+                    'top_score': self.questions[question_id].top_score,
+                    'correct_id': self.questions[question_id].correct_id,
+                })
+            )
+        else:
+            self._update_top_score(int(self.questions[question_id].cur_score))
+            self.write(
+                json.dumps({"verdict": "ERR", 'cur_score': self.questions[question_id].cur_score, 'answer': questions[question_id]['name']})
+            )
+
+        del self.questions[question_id]
